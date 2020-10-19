2774ff70ac092d1d5b46ce02904d6e32aa6ba054
laosen <laosen.@laosen>
2020-10-19 20:54:08 +0800

    制造冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("给爷上去");
 	System.out.println("哈啊哈");
+System.out.println("哈啊哈");
+System.out.println("哈啊哈");
 	
     }
 }
