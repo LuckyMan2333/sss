9ed5eeb192dddbdb37478df450df9181f4f5ba59
laosen <laosen.@laosen>
2020-10-19 20:38:43 +0800

    哼哼

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("傻逼乌龟");
-	System.out.println("捞逼乌龟");
+        System.out.println("哈哈啊哈鬲");
+	
     }
 }
