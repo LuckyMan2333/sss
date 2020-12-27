8b522f5a3aa08b5d7240199d232ddba7f75779ea
laosen <laosen.@laosen>
2020-12-27 12:06:36 +0800

    拉去代码练习

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -2,8 +2,8 @@ import org.junit.Test;
 
 public class Main {
 
-    @测试
-    公共 无效 estHelloWrold（）
-        System.out.println("那你是捞森的捞");
+    @Test
+    public void aaa() {
+        System.out.println("11111111");
     }
 }
